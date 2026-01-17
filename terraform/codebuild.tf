c4d7e7c6ebaf401ccd60f496a46992a0723e38cf
rahulbonkur <rahulbonkur@gmail.com>
2026-01-17 12:38:22 +0530

    Update codebuild.tf

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,17 +1,15 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_role" "codebuild_role" {
   name = "ai-chatbot-codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "codebuild.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
@@ -20,77 +18,46 @@ resource "aws_iam_role_policy" "codebuild_policy" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Resource = [
-          "*"
-        ]
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Resource = [
-          "*"
-        ]
-        Action = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:PutObject",
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart",
-          "ecs:UpdateService"
-        ]
-      }
-    ]
+    Statement = [{
+      Effect   = "Allow"
+      Action   = [
+        "logs:*",
+        "ecr:*",
+        "ecs:UpdateService",
+        "s3:GetObject"
+      ]
+      Resource = "*"
+    }]
   })
 }
 
 resource "aws_codebuild_project" "ai_chatbot" {
-  name          = "ai-chatbot"
-  description   = "Builds the AI Chatbot Docker image"
-  build_timeout = "5"
-  service_role  = aws_iam_role.codebuild_role.arn
+  name         = "ai-chatbot"
+  service_role = aws_iam_role.codebuild_role.arn
 
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
+  artifacts { type = "NO_ARTIFACTS" }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
 
     environment_variable {
       name  = "AWS_DEFAULT_REGION"
       value = var.aws_region
     }
-  }
 
-  logs_config {
-    cloudwatch_logs {
-      group_name  = "log-blog-build"
-      stream_name = "log-blog-build"
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = data.aws_caller_identity.current.account_id
     }
   }
 
   source {
-    type            = "GITHUB"
-    location        = "https://github.com/rahulbonkur/devops-final-project.git"
-    git_clone_depth = 1
-    buildspec       = "app1-ai-chatbot/buildspec.yml"
+    type      = "GITHUB"
+    location  = "https://github.com/rahulbonkur/devops-final-project.git"
+    buildspec = "app1-ai-chatbot/buildspec.yml"
   }
 
   source_version = "main"
